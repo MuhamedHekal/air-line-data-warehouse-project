92044ca610b9abe9208c4239d00c7be18e6712be
Mohamed Hekal <hhekal000@gmail.com>
2025-03-11 00:43:50 +0200

    schema_create

---
+++ Create_airLine_schema.sql
@@ -0,0 +1,17 @@
+-- run this file as a sys user
+CREATE USER airline IDENTIFIED BY airline;
+
+GRANT CREATE MATERIALIZED VIEW,
+      CREATE PROCEDURE,
+      CREATE SEQUENCE,
+      CREATE SESSION,
+      CREATE SYNONYM,
+      CREATE TABLE,
+      CREATE TRIGGER,
+      CREATE TYPE,
+      CREATE VIEW
+  TO airline;
+
+ALTER USER airline QUOTA UNLIMITED ON USERS;
+
+--drop user airline CASCADE;
