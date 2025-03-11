2496b11d0ea042eca078f2a6e67aec7fb6ae8daf
seiffmohamedd <seif500mohamed@gmail.com>
2025-03-11 12:16:26 +0200

    DDL Customer Care Updated

--- CustomerCare_DDL.sql
+++ CustomerCare_DDL.sql
@@ -1,39 +1,39 @@
 CREATE TABLE feedback_dim (
-    feedback_id INT PRIMARY KEY,
-    type VARCHAR(50),
-    description VARCHAR(500)
+    feedback_id NUMBER PRIMARY KEY,
+    type VARCHAR2(50),
+    description VARCHAR2(500)
 );
 
 CREATE TABLE employee_dim (
-    employee_id INT PRIMARY KEY,
-    employee_name VARCHAR(35),
+    employee_id NUMBER PRIMARY KEY,
+    employee_name VARCHAR2(35),
     employee_dateOfBirth DATE,
-    employee_gender VARCHAR(10),
-    employee_address VARCHAR(10),
-    employee_phone VARCHAR(20),
+    employee_gender VARCHAR2(10),
+    employee_address VARCHAR2(100),
+    employee_phone VARCHAR2(20),
     salary NUMBER(10,2)
 );
 
 CREATE TABLE date_dim (
-    date_id INT PRIMARY KEY,
-    year INT,
-    quarter INT,
-    month INT,
-    day_of_week INT,
-    day_of_month INT,
-    day_of_year INT,
-    week_of_year INT,
-    is_holiday BOOLEAN
+    date_id NUMBER PRIMARY KEY,
+    year NUMBER(4),
+    quarter NUMBER(1),
+    month NUMBER(2),
+    day_of_week NUMBER(1),
+    day_of_month NUMBER(2),
+    day_of_year NUMBER(3),
+    week_of_year NUMBER(2),
+    is_holiday NUMBER(1) CHECK (is_holiday IN (0,1))
 );
 
 CREATE TABLE CustomerCareFact (
-    customer_id INT,
-    date_id INT,
-    feedback_id INT,
-    employee_id INT,
-    interaction_type VARCHAR(50),
-    satisfaction_rate DECIMAL(5,2),
-    duration INT,
+    customer_id NUMBER,
+    date_id NUMBER,
+    feedback_id NUMBER,
+    employee_id NUMBER,
+    interaction_type VARCHAR2(50),
+    satisfaction_rate NUMBER(5,2),
+    duration NUMBER,
     FOREIGN KEY (date_id) REFERENCES date_dim(date_id),
     FOREIGN KEY (feedback_id) REFERENCES feedback_dim(feedback_id),
     FOREIGN KEY (employee_id) REFERENCES employee_dim(employee_id)
