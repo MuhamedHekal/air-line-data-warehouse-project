058b001d6f2bf7dd0146be1849af4bc22151ea65
seiffmohamedd <seif500mohamed@gmail.com>
2025-03-12 00:23:06 +0200

    Time Dim Script updated

--- profit_analysis_data.sql
+++ profit_analysis_data.sql
@@ -3,7 +3,6 @@ SELECT
     ROWNUM AS time_id,
     MOD(TRUNC((LEVEL - 1) / 2), 24) AS hour,  -- Cycles through 0-23 hours
     CASE MOD(LEVEL - 1, 2) WHEN 0 THEN 0 ELSE 30 END AS minutes,  -- Alternates 00 and 30
-    TRUNC((LEVEL - 1) / 48) + 1 AS month,  -- Cycles through 1-12 months
     CASE 
         WHEN MOD(TRUNC((LEVEL - 1) / 2), 24) = 0 THEN 'Midnight'
         WHEN MOD(TRUNC((LEVEL - 1) / 2), 24) BETWEEN 1 AND 5 THEN 'Early Morning'
@@ -13,7 +12,7 @@ SELECT
         WHEN MOD(TRUNC((LEVEL - 1) / 2), 24) BETWEEN 21 AND 23 THEN 'Night'
     END AS hour_description
 FROM DUAL
-CONNECT BY LEVEL <= 12 * 24 * 2;  -- 12 months * 24 hours * 2 (for 00 and 30 minutes)
+CONNECT BY LEVEL <= 24 * 2;  --  24 hours * 2 (for 00 and 30 minutes)
 
 COMMIT;
 
